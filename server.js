11dbf45ec232c682c016807a6d4c7ce91c19fda2
krushal29 <krushalbhadiyadra2908@gmail.com>
2025-05-25 15:46:50 +0530

    setup backend

---
+++ server.js
@@ -0,0 +1,30 @@
+import express from 'express'
+import connectDB from './Config/mongo.js';
+import dotenv from 'dotenv'
+import cors from 'cors'
+
+
+const port=80
+const app=express();
+
+
+
+app.use(express.json({limit:'50mb'}));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
+app.use(cors());
+dotenv.config();
+
+
+//Database connect
+connectDB();
+
+
+
+// For API
+// app.use('/api/user',userRoute);
+
+
+
+app.listen(port,()=>{
+    console.log("server is running "+port); 
+})
