0aa680873f8590805945dde05a30968c6f0d88da
krushal29 <krushalbhadiyadra2908@gmail.com>
2025-06-02 00:57:07 +0530

    Fetch All Data from Excel File

--- controllers/ExcelController.js
+++ controllers/ExcelController.js
@@ -35,12 +35,19 @@ const analyzeData = async (url) => {
 const uploadExcelFile = async (req, res) => {
     try {
         const UserEmail=req.user.id;
-        const user=await userDetails({email:UserEmail});
+
+
+        const user=await userDetails.findOne({email:UserEmail});
         const userId=user._id;
+
+
         const Files = req.file.path;
+
         if (!Files) {
             return res.status(400).json({ data: false, message: "Please upload an image!" });
         }
+
+
         const uploadFile = await cloudinary.uploader.upload(Files, {
             resource_type: "raw"
         })
@@ -48,8 +55,9 @@ const uploadExcelFile = async (req, res) => {
 
         const ProfileFile = uploadFile.secure_url;
         fs.unlinkSync(req.file.path);
-        const data = await analyzeData(ProfileFile)
 
+
+        const data = await analyzeData(ProfileFile)
         const ExcelData={
             user_id:userId,
             FileURl:ProfileFile,
@@ -67,11 +75,36 @@ const uploadExcelFile = async (req, res) => {
 
 
     } catch (e) {
-        res.status(500).json({ data: false, message: "Server Error.Please try again!!!", error: e })
+       return res.status(500).json({ data: false, message: "Server Error.Please try again!!!", error: e })
+    }
+}
+
+
+const ExcelAllData=async(req,res)=>{
+    try{
+        const UserEmail=req.user.id;
+
+        if (!UserEmail) {
+            return res.status(401).json({ data: false, message: "Unauthorized: User ID not found in request." });
+        }
+
+        const user=await userDetails.findOne({email:UserEmail});
+        const userId=user._id;
+
+        if(!user){
+            return res.status(404).json({data:false,message:"User not found!!!"})
+        }
+
+        const ExcelData=await ExcelDetails.find({user_id:userId})
+
+        return res.status(200).json({data:true,message:ExcelData})
+
+    }catch(e){
+        return res.status(500).json({data:false,message:"Server Error.Please try again!!!"})
     }
 }
 
 
 
 
-export default uploadExcelFile
+export {uploadExcelFile,ExcelAllData}
--- routes/Excel.js
+++ routes/Excel.js
@@ -1,13 +1,14 @@
 import { Router } from "express";
-import uploadExcelFile from "../controllers/ExcelController.js";
+import { uploadExcelFile, ExcelAllData } from "../controllers/ExcelController.js";
 import upload from "../middleware/multer.js";
 import verifyToken from "../middleware/verifyToken.js";
 
 
 
-const ExcelRouter=Router();
+const ExcelRouter = Router();
 
-ExcelRouter.post('/uploadExcelFile',upload.single("File"),verifyToken,uploadExcelFile)
+ExcelRouter.post('/uploadExcelFile', upload.single("File"), verifyToken, uploadExcelFile)
+ExcelRouter.get('/ExcelAllData', verifyToken, ExcelAllData)
 
 
 export default ExcelRouter;
