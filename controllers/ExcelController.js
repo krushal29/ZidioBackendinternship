0cdd45c8fe65eadab784095e13e0d60b75938867
krushal29 <krushalbhadiyadra2908@gmail.com>
2025-06-02 00:24:07 +0530

    analyzeData Database Created

--- controllers/ExcelController.js
+++ controllers/ExcelController.js
@@ -2,6 +2,8 @@ import { v2 as cloudinary } from 'cloudinary'
 import xlsx from 'xlsx'
 import fs from 'fs';
 import axios from 'axios'
+import userDetails from '../models/UserModel.js';
+import ExcelDetails from '../models/ExcelDataModel.js';
 
 
 
@@ -17,13 +19,10 @@ const analyzeData = async (url) => {
         const result = {};
 
         workbook.SheetNames.forEach(sheetName => {
-            console.log("SheetName",sheetName);
             const worksheet = workbook.Sheets[sheetName];
             const jsonData = xlsx.utils.sheet_to_json(worksheet, { defval: null });
             result[sheetName] = jsonData;
         });
-
-        console.log(result);
 
         return result
 
@@ -35,6 +34,9 @@ const analyzeData = async (url) => {
 
 const uploadExcelFile = async (req, res) => {
     try {
+        const UserEmail=req.user.id;
+        const user=await userDetails({email:UserEmail});
+        const userId=user._id;
         const Files = req.file.path;
         if (!Files) {
             return res.status(400).json({ data: false, message: "Please upload an image!" });
@@ -42,10 +44,27 @@ const uploadExcelFile = async (req, res) => {
         const uploadFile = await cloudinary.uploader.upload(Files, {
             resource_type: "raw"
         })
+
+
         const ProfileFile = uploadFile.secure_url;
         fs.unlinkSync(req.file.path);
-          const data= await analyzeData(ProfileFile)
-        res.json({ success: true, ProfileFile, asset_id: uploadFile.asset_id, orignalFileName: uploadFile.original_filename });
+        const data = await analyzeData(ProfileFile)
+
+        const ExcelData={
+            user_id:userId,
+            FileURl:ProfileFile,
+            FileName:uploadFile.display_name,
+            FileSize:uploadFile.bytes,
+            asset_id:uploadFile.asset_id,
+            ExcelData:data
+        }
+
+        const response=await ExcelDetails.insertOne(ExcelData);
+        
+        if(response){
+           return res.json({ success: true, ProfileFile, asset_id: uploadFile.asset_id, display_name: uploadFile.display_name });
+        }
+
 
     } catch (e) {
         res.status(500).json({ data: false, message: "Server Error.Please try again!!!", error: e })
---
+++ models/ExcelDataModel.js
@@ -0,0 +1,34 @@
+import mongoose from "mongoose";
+
+const ExcelDataSchema = new mongoose.Schema({
+    user_id: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true
+    },
+    FileURl: {
+        type: String,
+        required: true
+    },
+    FileName: {
+        type: String,
+        required: true
+    },
+    FileSize: {
+        type: Number,
+        required: true
+    },
+    asset_id: {
+        type: String,
+    },
+    ExcelData: {
+        type: Object,
+        default: {},
+    }
+
+
+}, { collection: "ExcelDetails", timestamps: true })
+
+
+const ExcelDetails = mongoose.model("Excel", ExcelDataSchema);
+
+export default ExcelDetails;
